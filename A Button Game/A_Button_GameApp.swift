c225d5486ca5d73632b9c03645f449b65d5c8599
LuckDuracell <lukedru@yahoo.com>
2021-08-17 21:11:31 -0700

    Initial Commit

---
+++ A Button Game/A_Button_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  A_Button_GameApp.swift
+//  A Button Game
+//
+//  Created by Luke Drushell on 8/17/21.
+//
+
+import SwiftUI
+
+@main
+struct A_Button_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
